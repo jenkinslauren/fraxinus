4c8faf7d8bf341fa4bab1521c3ac041cef142a5b
Lauren Jenkins <lauren.jenkins@my.wheaton.edu>
2022-07-06 15:43:03 -0500

    env data updates

--- model_evaluations.R
+++ model_evaluations.R
@@ -11,8 +11,9 @@ library(enmSdm)
 library(geosphere)
 library(sp)
 library(dismo)
+library(rgeos)
 
-cluster <- F
+cluster <- T
 
 if (cluster == T) { # constants for running on cluster
   args <- commandArgs(TRUE)
@@ -31,7 +32,8 @@ if (cluster == T) { # constants for running on cluster
   speciesList <- unlist(speciesList)
   
   baseFolder <- '/mnt/research/TIMBER/PVMvsENM/'
-  setwd('/mnt/research/TIMBER/PVMvsENM')
+  setwd(paste0(baseFolder, genus, '/in/'))
+  
 } else {
   evalType <- 'random'
   
@@ -52,170 +54,168 @@ ll <- c('longitude', 'latitude')
 pc <- 5
 predictors <- c(paste0('pca', 1:pc))
 
-if(evalType == 'random') {
-  for (gcm in gcmList) {
-    print(paste0("GCM = ", gcm))
+for (gcm in gcmList) {
+  print(paste0("GCM = ", gcm))
+  
+  a <- data.frame(c(seq(1:5)))
+  c <- data.frame(c(seq(1:5)))
+  colnames(a)[1] <- colnames(c)[1] <- 'fold #'
+  
+  for(sp in speciesList) {
+    print(paste0("Species = ", sp))
     
-    a <- data.frame(c(seq(1:5)))
-    c <- data.frame(c(seq(1:5)))
-    colnames(a)[1] <- colnames(c)[1] <- 'fold #'
+    speciesAb_ <- sub("(.{4})(.*)", "\\1_\\2", 
+                      paste0(substr(sp,1,4), toupper(substr(sub("^\\S+\\s+", '', sp),1,1)), 
+                             substr(sub("^\\S+\\s+", '', sp),2,4)))
     
-    for(sp in speciesList) {
-      print(paste0("Species = ", sp))
+    # set constants for retrieving background sites #
+    bgFileName <- paste0(baseFolder, 
+                         'background_sites/Random Background Sites across Study Region.Rdata')
+    load(bgFileName) # load bg sites in calibration region
+    
+    # set constants for retrieving model objects #
+    modelFileName <- paste0('./models/predictions/', speciesAb_, 
+                            '/GCM_', gcm, '_PC', pc, '.rData')
+    load(modelFileName) # load model object, bg, and records for given species
+    
+    evalFolderName <- paste0('./models/model_evaluations/', speciesAb_, 
+                             '/', evalType, '_k_folds/')
+    if(!dir.exists(evalFolderName)) dir.create(evalFolderName, recursive = TRUE, 
+                                               showWarnings = FALSE)
+    
+    evalFolderName <- paste0(evalFolderName, gcm, '/')
+    if(!dir.exists(evalFolderName)) dir.create(evalFolderName, recursive = TRUE, 
+                                               showWarnings = FALSE)
+    
+    # variable to store auc & cbi output
+    auc <- cbi <- rep(NA, 5)
+    
+    if (evalType == 'random') {
+      kPres <- kfold(records, k = 5) # k-folds for presences
+      kBg <- kfold(bg, k = 5) # k-folds for backgrounds
       
-      speciesAb_ <- sub("(.{4})(.*)", "\\1_\\2", 
-                        paste0(substr(sp,1,4), toupper(substr(sub("^\\S+\\s+", '', sp),1,1)), 
-                               substr(sub("^\\S+\\s+", '', sp),2,4)))
+      ### code for visualizing folds ###
+      # plot(range, main = paste0(sp, ', k-fold #1'))
+      # points(records$longitude, records$latitude)
+      # points(records$longitude[kPres==1],
+      #        records$latitude[kPres==1],
+      #        bg='red',
+      #        pch=21
+      # )
       
-      # set constants for retrieving background sites #
-      bgFileName <- paste0(baseFolder, 
-                           'background_sites/Random Background Sites across Study Region.Rdata')
-      load(bgFileName) # load bg sites in calibration region
+      # legend('topright',
+      #        legend=c('Training presence', 'Test presence'),
+      #        pch=c(1, 16),
+      #        col=c('black', 'red'),
+      #        bg='white',
+      #        cex=0.8
+      # )
       
-      modelFileName <- paste0('./models/predictions/', speciesAb_, 
-                              '/GCM_', gcm, '_PC', pc, '.rData')
-      load(modelFileName) # load model object, bg, and records for given species
-      
-      evalFolderName <- paste0('./models/model_evaluations/', speciesAb_, 
-                               '/', evalType, '_k_folds/')
-      if(!dir.exists(evalFolderName)) dir.create(evalFolderName, recursive = TRUE, 
-                                                 showWarnings = FALSE)
-      
-      evalFolderName <- paste0(evalFolderName, gcm, '/')
-      if(!dir.exists(evalFolderName)) dir.create(evalFolderName, recursive = TRUE, 
-                                                 showWarnings = FALSE)
-      
-      # variable to store auc & cbi output
-      auc <- cbi <- rep(NA, 5)
-      
-      if (evalType == 'random') {
-        kPres <- kfold(records, k = 5) # k-folds for presences
-        kBg <- kfold(bg, k = 5) # k-folds for backgrounds
+      for(i in 1:5) { # for each k-fold
+        print(paste0('K-fold ', i, ':'))
         
-        ### code for visualizing folds ###
-        # plot(range, main = paste0(sp, ', k-fold #1'))
-        # points(records$longitude, records$latitude)
-        # points(records$longitude[kPres==1],
-        #        records$latitude[kPres==1],
-        #        bg='red',
-        #        pch=21
-        # )
+        # create training data, with presences/absences vector of 0/1 with all points 
+        # EXCEPT the ones in the fold
+        envData <- rbind(records[kPres != i, predictors], bg[kBg != i, predictors])
+        presBg <- c(rep(1, sum(kPres != i)), rep(0, sum(kBg != i)))
+        trainData <- cbind(presBg, envData)
         
-        # legend('topright',
-        #        legend=c('Training presence', 'Test presence'),
-        #        pch=c(1, 16),
-        #        col=c('black', 'red'),
-        #        bg='white',
-        #        cex=0.8
-        # )
+        model_tune <- enmSdm::trainMaxNet(data = trainData, resp = 'presBg', 
+                                          classes = 'lpq', out = c('models', 'tuning'))
+        model <- model_tune$models[[1]]
         
-        for(i in 1:5) { # for each k-fold
-          print(paste0('K-fold ', i, ':'))
-          
-          # create training data, with presences/absences vector of 0/1 with all points 
-          # EXCEPT the ones in the fold
-          envData <- rbind(records[kPres != i, predictors], bg[kBg != i, predictors])
-          presBg <- c(rep(1, sum(kPres != i)), rep(0, sum(kBg != i)))
-          trainData <- cbind(presBg, envData)
-          
-          model_tune <- enmSdm::trainMaxNet(data = trainData, resp = 'presBg', 
-                                            classes = 'lpq', out = c('models', 'tuning'))
-          model <- model_tune$models[[1]]
-          
-          # predict presences & background sites
-          predPres <- raster::predict(model, 
-                                      newdata = records[kPres == i,],
-                                      clamp = F,
-                                      type = 'cloglog')
-          predBg <- raster::predict(model, 
-                                    newdata = bg[kPres == i,],
+        # predict presences & background sites
+        predPres <- raster::predict(model, 
+                                    newdata = records[kPres == i,],
                                     clamp = F,
                                     type = 'cloglog')
-          
-          save(model, model_tune, predPres, predBg, kPres, kBg,
-               file = paste0(evalFolderName, '/model_', i, '.Rdata'), overwrite = T)
-          
-        }
-      } else if (evalType == 'geo') {
-        # create g-folds
-        gPres <- geoFold(x = records, k = 5, minIn = 5, minOut = 10, longLat = ll)
+        predBg <- raster::predict(model, 
+                                  newdata = bg[kPres == i,],
+                                  clamp = F,
+                                  type = 'cloglog')
         
-        # now, we have our folds, but we want to divide the bg sites into 
-        # folds based on where they are in relation to records
+        save(model, model_tune, predPres, predBg, kPres, kBg,
+             file = paste0(evalFolderName, '/model_', i, '.Rdata'), overwrite = T)
         
-        # initialize vectors to store g-fold assignments
-        gTestBg <- rep(NA, nrow(bg))
-        
-        # convert records to sp object for gDistance function
-        sp.records <- records
-        coordinates(sp.records) <- ~longitude + latitude
-        sp.randomBg <- bg
-        coordinates(sp.randomBg) <- ~longitude + latitude
-        
-        # divide bg sites between training & test
-        nearest <- apply(gDistance(sp.records, sp.randomBg, byid = T), 1, which.min)
-        
-        for (k in 1:nrow(bg)) {
-          gTestBg[k] <- gPres[nearest[k]]
-        }
-        
-        for (m in 1:5) { # make training data frame with predictors 
-          # and vector of 1/0 for presence/background
-          
-          envData <- rbind(
-            records[gPres!=m, predictors],
-            bg[gTestBg!=m, predictors]
-          )
-          
-          presBg <- c(rep(1, sum(gPres!=m)), rep(0, sum(gTestBg!=m)))
-          trainData <- cbind(presBg, envData)
-          
-          # maxent model
-          model_tune <- enmSdm::trainMaxNet(data = trainData, resp = 'presBg', 
-                                            classes = 'lpq', out = c('models', 'tuning'))
-          model <- model_tune$models[[1]]
-          
-          # predict presences & background sites
-          predPres <- raster::predict(model, 
-                                      newdata = records[gPres == m,],
-                                      clamp = F,
-                                      type = 'cloglog')
-          predBg <- raster::predict(model, 
-                                    newdata = bg[gTestBg == m,],
-                                    clamp = F,
-                                    type = 'cloglog')
-          
-          save(model, predPres, predBg, gPres, gTestBg, model_tune, 
-               file = paste0(evalFolderName, '/model ', m, '.Rdata'), compress = T)
-          
-        }
+      }
+    } else if (evalType == 'geo') {
+      # create g-folds
+      gPres <- geoFold(x = records, k = 5, minIn = 5, minOut = 10, longLat = ll)
+      
+      # now, we have our folds, but we want to divide the bg sites into 
+      # folds based on where they are in relation to records
+      
+      # initialize vectors to store g-fold assignments
+      gTestBg <- rep(NA, nrow(bg))
+      
+      # convert records to sp object for gDistance function
+      sp.records <- records
+      coordinates(sp.records) <- ~longitude + latitude
+      sp.randomBg <- bg
+      coordinates(sp.randomBg) <- ~longitude + latitude
+      
+      # divide bg sites between training & test
+      nearest <- apply(gDistance(sp.records, sp.randomBg, byid = T), 1, which.min)
+      
+      for (k in 1:nrow(bg)) {
+        gTestBg[k] <- gPres[nearest[k]]
       }
       
-      # evaluate
-      thisEval <- evaluate(p = as.vector(predPres), a = as.vector(predBg))
-      thisAuc <- thisEval@auc
-      thisCbi <- contBoyce(pres = predPres, bg = predBg)
-      
-      # print(paste('AUC = ', round(thisAuc, 2), ' | CBI = ', round(thisCbi, 2)))
-      
-      auc[m] <- thisAuc
-      cbi[m] <- thisCbi
+      for (m in 1:5) { # make training data frame with predictors 
+        # and vector of 1/0 for presence/background
+        
+        envData <- rbind(
+          records[gPres!=m, predictors],
+          bg[gTestBg!=m, predictors]
+        )
+        
+        presBg <- c(rep(1, sum(gPres!=m)), rep(0, sum(gTestBg!=m)))
+        trainData <- cbind(presBg, envData)
+        
+        # maxent model
+        model_tune <- enmSdm::trainMaxNet(data = trainData, resp = 'presBg', 
+                                          classes = 'lpq', out = c('models', 'tuning'))
+        model <- model_tune$models[[1]]
+        
+        # predict presences & background sites
+        predPres <- raster::predict(model, 
+                                    newdata = records[gPres == m,],
+                                    clamp = F,
+                                    type = 'cloglog')
+        predBg <- raster::predict(model, 
+                                  newdata = bg[gTestBg == m,],
+                                  clamp = F,
+                                  type = 'cloglog')
+        
+        save(model, predPres, predBg, gPres, gTestBg, model_tune, 
+             file = paste0(evalFolderName, '/model ', m, '.Rdata'), compress = T)
+        
+      }
     }
     
-    save(auc, cbi, file = paste0(evalFolderName, '/auc_cbi_vals.Rdata'))
+    # evaluate
+    thisEval <- evaluate(p = as.vector(predPres), a = as.vector(predBg))
+    thisAuc <- thisEval@auc
+    thisCbi <- contBoyce(pres = predPres, bg = predBg)
     
-    a <- cbind(a, auc)
-    c <- cbind(c, cbi)
-    n <- ncol(a)
-    colnames(a)[n] <- colnames(c)[n] <- sp
+    # print(paste('AUC = ', round(thisAuc, 2), ' | CBI = ', round(thisCbi, 2)))
     
+    auc[m] <- thisAuc
+    cbi[m] <- thisCbi
   }
   
-  write.xlsx(a, file = paste0('./models/model_evaluations/', evalType, '_evals.xlsx'), 
-             sheetName = paste0(gcm, '_auc'), append = T, row.names = F)
-  write.xlsx(c, file = paste0('./models/model_evaluations/', evalType, '_evals.xlsx'), 
-             sheetName = paste0(gcm, '_cbi'), append = T, row.names = F)
+  save(auc, cbi, file = paste0(evalFolderName, '/auc_cbi_vals.Rdata'))
   
-  save(a, c, file = paste0('./models/model_evaluations/', gcm, '_evals.Rdata'))
+  a <- cbind(a, auc)
+  c <- cbind(c, cbi)
+  n <- ncol(a)
+  colnames(a)[n] <- colnames(c)[n] <- sp
   
 }
+
+write.xlsx(a, file = paste0('./models/model_evaluations/', evalType, '_evals.xlsx'), 
+           sheetName = paste0(gcm, '_auc'), append = T, row.names = F)
+write.xlsx(c, file = paste0('./models/model_evaluations/', evalType, '_evals.xlsx'), 
+           sheetName = paste0(gcm, '_cbi'), append = T, row.names = F)
+
+save(a, c, file = paste0('./models/model_evaluations/', gcm, '_evals.Rdata'))
